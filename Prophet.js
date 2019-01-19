b2ae4d104534e957c8508b4719ac61f847d214ff
Joseph Camacho <camacho.joseph@gmail.com>
2019-01-19 15:35:47 -0700

    Add files via upload

--- Prophet.js
+++ Prophet.js
@@ -1,29 +1,43 @@
 import {SPECS} from 'battlecode';
 import nav from './nav.js';
 
-var Crusader = {};
-Crusader.attackDirs = [[-4, 0], [-3, -2], [-3, -1], [-3, 0], [-3, 1], [-3, 2], [-2, -3], [-2, -2], [-2, -1], [-2, 0], [-2, 1], [-2, 2], [-2, 3], [-1, -3], [-1, -2], [-1, -1], [-1, 0], [-1, 1], [-1, 2], [-1, 3], [0, -4], [0, -3], [0, -2], [0, -1], [0, 1], [0, 2], [0, 3], [0, 4], [1, -3], [1, -2], [1, -1], [1, 0], [1, 1], [1, 2], [1, 3], [2, -3], [2, -2], [2, -1], [2, 0], [2, 1], [2, 2], [2, 3], [3, -2], [3, -1], [3, 0], [3, 1], [3, 2], [4, 0]];
-Crusader.moveDirs = [[-3, 0], [-2, -2], [-2, -1], [-2, 0], [-2, 1], [-2, 2], [-1, -2], [-1, -1], [-1, 0], [-1, 1], [-1, 2], [0, -3], [0, -2], [0, -1], [0, 1], [0, 2], [0, 3], [1, -2], [1, -1], [1, 0], [1, 1], [1, 2], [2, -2], [2, -1], [2, 0], [2, 1], [2, 2], [3, 0]];
+var Prophet = {};
+Prophet.attackDirs = [[-4, 0], [-3, -2], [-3, -1], [-3, 0], [-3, 1], [-3, 2], [-2, -3], [-2, -2], [-2, -1], [-2, 0], [-2, 1], [-2, 2], [-2, 3], [-1, -3], [-1, -2], [-1, -1], [-1, 0], [-1, 1], [-1, 2], [-1, 3], [0, -4], [0, -3], [0, -2], [0, -1], [0, 1], [0, 2], [0, 3], [0, 4], [1, -3], [1, -2], [1, -1], [1, 0], [1, 1], [1, 2], [1, 3], [2, -3], [2, -2], [2, -1], [2, 0], [2, 1], [2, 2], [2, 3], [3, -2], [3, -1], [3, 0], [3, 1], [3, 2], [4, 0]];
+Prophet.moveDirs = [[-3, 0], [-2, -2], [-2, -1], [-2, 0], [-2, 1], [-2, 2], [-1, -2], [-1, -1], [-1, 0], [-1, 1], [-1, 2], [0, -3], [0, -2], [0, -1], [0, 1], [0, 2], [0, 3], [1, -2], [1, -1], [1, 0], [1, 1], [1, 2], [2, -2], [2, -1], [2, 0], [2, 1], [2, 2], [3, 0]];
 
 let end_goals = [];
 let temp_goals = [];
-let counter = 5;
 let mapWidth;
 let mapHeight;
-Crusader.turn = function turn(_this) {
+Prophet.turn = function turn(_this) {
 	if(_this.me.turn === 1) {
 		mapWidth = _this.map[0].length;
 		mapHeight = _this.map.length;
 	}
 	let visibleRobotMap = _this.getVisibleRobotMap();
-	let friends = visibleRobots.reduce(robot => robot.team === _this.me.team);
-	let enemies = visibleRobots.reduce(robot => robot.team !== _this.me.team);
-	let attackableEnemies = enemies.reduce(enemy => nav.distSquared([enemy.x, enemy.y], [_this.me.x, _this.me.y]) <= 16);
+	let friends = visibleRobots.filter(robot => robot.team === _this.me.team);
+	let enemies = visibleRobots.filter(robot => robot.team !== _this.me.team);
+	//enemies it can attack
+	let attackableEnemies = enemies.filter(function _(enemy) {
+		let d2 = nav.distSquared([enemy.x, enemy.y], [_this.me.x, _this.me.y]);
+		return (d2 <= 64 && d2 >= 16);
+	});
+	let closeEnemies = enemies.filter(enemy => nav.distSquared([enemy.x, enemy.y], [_this.me.x, _this.me.y]) <= 26);
 	
-	//make crusaders chase enemies they see
+	//make Prophets chase enemies they see
 	if(enemies.length>0) {
 		temp_goals = enemies.map(enemy => [enemy.x, enemy.y]);
 	}
+	//run away from close enemies
+	if(closeEnemies.length > 0) {
+		let closeEnemyLocations = closeEnemies.map(enemy => [enemy.x, enemy.y]);
+		let bestMoves = nav.runAwayDirs([_this.me.x, _this.me.y], closeEnemyLocations, Prophet.moveDirs, _this.map, visibleRobotMap);
+		if(bestMoves.length>0) {
+			let dir = bestMoves[Math.floor(Math.random()*bestMoves.length)];
+			return _this.move(dir[0], dir[1]);
+		}
+	}
+	
 	//attack weakest enemy in range
 	if(attackableEnemies.length>0) {
 		let weakest = attackableEnemies[0];
@@ -44,23 +58,18 @@ Crusader.turn = function turn(_this) {
 	friends.forEach(friend => map_copy[friend.y][friend.x] = false);
 	
 	if(temp_goals.length>0) {
-		let bfsMap = nav.breadthFirstSearch(temp_goals, map_copy, Crusader.moveDirs, _this);
+		let bfsMap = nav.breadthFirstSearch(temp_goals, map_copy, Prophet.moveDirs, _this);
 		if(bfsMap[_this.me.y][_this.me.x].length > 0) {
 			let newLocation = bfsMap[_this.me.y][_this.me.x][Math.floor(Math.random()*bfsMap[_this.me.y][_this.me.x].length)];
 			return _this.move(newLocation - _this.me.x, newLocation - _this.me.y);
 		}
 	} else if(end_goals.length>0) {
-		let bfsMap = nav.breadthFirstSearch(end_goals, map_copy, Crusader.moveDirs, _this);
+		let bfsMap = nav.breadthFirstSearch(end_goals, map_copy, Prophet.moveDirs, _this);
 		if(bfsMap[_this.me.y][_this.me.x].length > 0) {
 			let newLocation = bfsMap[_this.me.y][_this.me.x][Math.floor(Math.random()*bfsMap[_this.me.y][_this.me.x].length)];
 			return _this.move(newLocation - _this.me.x, newLocation - _this.me.y);
 		}
-	} else {
-		counter--;
-		if(counter<0) {
-			temp_goals.push(nav.closestPassableLocation([Math.floor(Math.random()*mapWidth), Math.floor(Math.random()*mapHeight)], passable_map, _this));
-		}
 	}
 	
 }
-export default Crusader;
+export default Prophet;
