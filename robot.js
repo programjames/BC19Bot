1fbd2bd815f0598a8acca056eeae4699f767b347
James Camacho <35083764+programjames@users.noreply.github.com>
2019-01-19 17:23:56 -0700

    Add files via upload

--- robot.js
+++ robot.js
@@ -4,16 +4,49 @@ import {
 } from 'battlecode';
 
 
+import Scout from './Scout.js';
+import Miner from './Miner.js';
 import Preacher from './Preacher.js';
 import Crusader from './Crusader.js';
 import Prophet from './Prophet.js';
 import Castle from './Castle.js';
 
+const buildDirs = [[1,0],[-1,0],[0,1],[0,-1],[1,1],[1,-1],[-1,1],[-1,-1]];
+var amScout = false;
+var amKarb = true;
+var amFuel = false;
+
+var KARB_MINER = 1;
+var FUEL_MINER = 2;
+
 class MyRobot extends BCAbstractRobot {
     turn() {
 		this.log(""+this.me.turn);
 		if(this.me.unit === SPECS.PILGRIM) {
-			
+			if(this.me.turn === 1){
+				let visibleRobotMap = this.getVisibleRobotMap();
+				for(let i = 0; i<buildDirs.length; i++){
+					let x = buildDirs[i][0]+this.me.x;
+					let y = buildDirs[i][1]+this.me.y;
+					if(visibleRobotMap[y][x]>0){
+						let robot = this.getRobot(visibleRobotMap[y][x]);
+						if(robot.team === this.me.team && robot.unit === SPECS.CASTLE && robot.signal!==-1){
+							let message = robot.signal;
+							if(message === FUEL_MINER){
+								amKarb = false;
+								amFuel = true;
+							}
+							else if(message === KARB_MINER){
+								// pass
+							}
+							else{
+								amKarb = false;
+								amScout = true;
+							}
+						}
+					}
+				}
+			}
 		} else if(this.me.unit === SPECS.PREACHER) {
 			return Preacher.turn(this);
 		} else if (this.me.unit === SPECS.PROPHET) {
